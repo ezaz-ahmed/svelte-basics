fb6073227d83b5d32395a446e2f72399b00a02c9
Ezaz Ahmed <ezaznahmed@gmail.com>
2021-10-14 19:47:49 +0600

    Svelte-basics

---
+++ src/App.svelte
@@ -0,0 +1,44 @@
+<script>
+import FeedbackForm from "./components/FeedbackForm.svelte";
+
+
+	import FeedbackList from "./components/FeedbackList.svelte";
+	import FeedbackStats from './components/Feedbackstats.svelte';
+
+	let feedBack = [
+		{
+    		id: 1,
+    		rating: 10,
+    		text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+  		},
+		{
+			id: 2,
+			rating: 9,
+			text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+		},
+		{
+			id: 3,
+			rating: 8,
+			text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+		}
+	]
+
+	const deleteFeedback = (e) => {
+		const itemId = e.detail
+		feedBack = feedBack.filter(fb => fb.id !== itemId)
+	}
+
+	$: count = feedBack.length
+	$: average = feedBack.reduce((prev, {rating}) => prev + rating ,0) / count
+
+</script>
+
+<main class="container">
+	<FeedbackForm />
+	<FeedbackStats {count} {average} />
+	<FeedbackList {feedBack} on:delete-feedback={deleteFeedback} />
+</main>
+
+<style>
+	
+</style>
---
+++ src/components/FeedbackList.svelte
@@ -0,0 +1,10 @@
+<script>
+    import FeedbackItem from "./FeedbackItem.svelte";
+    export let feedBack = []
+
+    console.log(feedBack)
+</script>
+
+{#each feedBack as fb (fb.id)}
+    <FeedbackItem item={fb} on:delete-feedback />
+{/each}
